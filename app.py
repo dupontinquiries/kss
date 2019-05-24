bc03e3a5c41e610ecc846f8f7a249f4bea0c8d89
dupontinquiries <43118393+dupontinquiries@users.noreply.github.com>
2019-05-23 23:50:08 -0400

    working multi-chunk analysis

--- app.py
+++ app.py
@@ -14,59 +14,87 @@ from math import *
 def convo_splice(i):
     print("func")
 
+def get_frame_rate(filename):
+    if not os.path.exists(filename):
+        sys.stderr.write("ERROR: filename %r was not found!" % (filename,))
+        return -1
+    out = subprocess.check_output(["ffprobe",filename,"-v","0","-select_streams","v","-print_format","flat","-show_entries","stream=r_frame_rate"])
+    rate = out.split('=')[1].strip()[1:-1].split('/')
+    if len(rate)==1:
+        return float(rate[0])
+    if len(rate)==2:
+        return float(rate[0])/float(rate[1])
+    return -1
+
 # double filter process
-def process_in_groups(i):
+def process_in_groups(i, mod, c_l, spread):
+    if os.path.exists("final\\processed_output_from_" + i + ".mp4"):
+        os.remove("final\\processed_output_from_" + i + ".mp4")
+    if os.path.exists("final\\filtered_and_processed_output_from_" + i + ".mp4"):
+        os.remove("final\\filtered_and_processed_output_from_" + i + ".mp4")
     i = str(i.replace(".mp4", ""))
     #
     input = ffmpeg.input(i + ".mp4")
-    movie = VideoFileClip(i + ".mp4")
     a = input['a']
     a = a.filter('highpass', 400).filter("lowpass", 15000).filter("loudnorm")
     v = input['v']
+    movie = VideoFileClip(i + ".mp4")
     output = ffmpeg.output(a, "tmp_a_from_" + i + ".wav")
     ffmpeg.run(output)
     a = AudioSegment.from_wav("tmp_a_from_" + i + ".wav")
     if os.path.exists("tmp_a_from_" + i + ".wav"):
         os.remove("tmp_a_from_" + i + ".wav")
-    chunk_length_ms = 700  # 4000 is best
+    chunk_length_ms = c_l  # 4000 is best
     chunk_length_s = chunk_length_ms/1000
     chunks_a = make_chunks(a, chunk_length_ms)
     tc_v = []
     list_of_db = []
     list_of_db_solo = []
-    spread = 5
     spread_calc = int(((spread - 1) / 2))
     #get start
-    for z in range(0, spread_calc):
-        list_of_db_solo.append(chunks_a[z])
-    list_of_db.append(sum(chunks_a[0:spread]))
+    db_arr = 0
+    for q_1 in range(0, 2 * spread_calc):
+        db_arr += chunks_a[q_1].dBFS
+    db = db_arr / spread
+    for z_init in range(0, spread_calc - 1):
+        list_of_db.append(db)
+        list_of_db_solo.append(chunks_a[z_init].dBFS)
+        print(str(z_init) + " run " + "start")
     #middle
-    for z in range(spread_calc, len(chunks_a) - 1 - spread_calc):
+    for z_mid in range(spread_calc, len(chunks_a) - 1 - spread_calc):
         db = 0
         db_arr = 0
-        for q in range(z - spread_calc, z + 1 + spread_calc):
-            db_arr += chunks_a[q].dBFS
+        for q_2 in range(z_mid - spread_calc, z_mid + 1 + spread_calc):
+            db_arr += chunks_a[q_2].dBFS
         db = db_arr/spread
-        db_solo = chunks_a[z].dBFS
+        db_solo = chunks_a[z_mid].dBFS
         list_of_db.append(db)
         list_of_db_solo.append(db_solo)
-        print(str(db_solo))
+        print(str(z_mid) + " run " + "middle")
     #get end
-    for z in range(len(chunks_a) - 1 - spread_calc, len(chunks_a) - 1):
-        list_of_db_solo.append(chunks_a[z])
-    list_of_db.append(sum(chunks_a[len(chunks_a) - 1 - spread:len(chunks_a) - 1]))
+    db_arr = 0
+    for q_3 in range(len(chunks_a) - 1 - spread_calc, len(chunks_a) - 1):
+        db_arr += chunks_a[q_3].dBFS
+    db = db_arr / spread
+    for z_end in range(len(chunks_a) - (2 * spread_calc), len(chunks_a) - 1):
+        list_of_db.append(db)
+        list_of_db_solo.append(chunks_a[z_end].dBFS)
+        print(str(z_end) + " run " + "end")
     max_db = max(list_of_db_solo)
-    thresh = 1.2 * max_db
+    thresh = mod * max_db
     print("max_db: " + str(max_db))
     print("thresh: " + str(thresh))
+    fps = movie.fps
+    print("fps: " + str(len(list_of_db)))
     if len(chunks_a) > 1:
-        for x in range(0, len(chunks_a) - 1 - spread_calc):
+        for x in range(0, len(chunks_a) - 1):
             # op1 - harsh analysis on long pieces
             raw = list_of_db[x] # group
             raw_solo = list_of_db_solo[x]  # group
-            if raw > thresh or raw_solo > thresh * .85: # -27 works best, -36
-                print("subclips: " + str(tc_v))
-                tc_v.append(movie.subclip((x * chunk_length_s), (x * chunk_length_s + chunk_length_s)))
+            if raw_solo > thresh or raw_solo > (thresh * .90):
+                #print("subclips: " + str(tc_v))
+                tmp = movie.subclip((x * chunk_length_s), ((x+1) * chunk_length_s))
+                tc_v.append(tmp) #this is wrong
                 print("vol_a = " + str(raw))
             # op2 - lenient analysis of shorter lengths
         # come back and check if deleting first skipped ones is necessary
@@ -74,12 +102,23 @@ def process_in_groups(i):
     processed = concatenate(tc_v)
     print("concat: " + str(processed))
     processed.write_videofile("final\\processed_output_from_" + i + ".mp4")
-    if not processed:
-        processed = ffmpeg.output([], [], "final\\processed_output_from_" + i + ".mp4")
-        ffmpeg.run(processed)
     ret = ffmpeg.input("final\\processed_output_from_" + i + ".mp4")
-    #if os.path.exists("processed_output_from_" + i + ".mp4"):
-        #os.remove("processed_output_from_" + i + ".mp4")
+    movie_width = movie.w
+    movie_height = movie.h
+    desired_height = 1350
+    scale_factor = movie_height / desired_height
+    base_a = ret['a'].filter("loudnorm").filter("acompressor")  # .filter("dynaudnorm")
+    ffmpeg.run(ffmpeg.output(base_a, "final\\processed_audio_from" + i + ".wav"))
+    new_audio = (ffmpeg.input("final\\processed_audio_from" + i + ".wav"))
+    base_v = ret['v'].filter("atadenoise").filter('crop', x=0*1080*scale_factor, y=0, w=1080*scale_factor, h=1350*scale_factor)
+    output = ffmpeg.output(base_v, new_audio, "final\\filtered_and_processed_output_from_" + i + ".mp4")
+    ffmpeg.run(output)
+    ret = ffmpeg.input("final\\filtered_and_processed_output_from_" + i + ".mp4")
+
+
+    #if os.path.exists("final\\processed_output_from_" + i + ".mp4"): os.remove("final\\processed_output_from_" + i + ".mp4") if os.path.exists("final\\filtered_and_processed_output_from_" + i + ".mp4"): os.remove("final\\filtered_and_processed_output_from_" + i + ".mp4")
+
+
     return ret
 
 
@@ -142,10 +181,16 @@ def process(i):
     processed = concatenate(tc_v)
     print("concat: " + str(processed))
     processed.write_videofile("final\\processed_output_from_" + i + ".mp4")
-    if not processed:
-        processed = ffmpeg.output([], [], "final\\processed_output_from_" + i + ".mp4")
-        ffmpeg.run(processed)
     ret = ffmpeg.input("final\\processed_output_from_" + i + ".mp4")
+    movie_width = movie.width
+    movie_height = movie.height
+    desired_height = 1350
+    scale_factor = movie_height/desired_height
+    base_a = ret['a'].filter("loudnorm").filter("acompressor")  # .filter("dynaudnorm")
+    base_v = ret['v'].filter("atadenoise").filter('scale', w=(str(movie_width*scale_factor)),
+                                                  h=(str(movie_height*scale_factor))).filter('crop', w='1080', h='1350')
+    ffmpeg.run(base_v, base_a, "final\\filtered_and_processed_output_from_" + i + ".mp4")
+    ret = ffmpeg.input("final\\filtered_and_processed_output_from_" + i + ".mp4")
     #if os.path.exists("processed_output_from_" + i + ".mp4"):
         #os.remove("processed_output_from_" + i + ".mp4")
     return ret
@@ -160,12 +205,12 @@ vid_arr = create_video_list(dir)
 vid_arr.sort(key=lambda x: os.path.getmtime(x))
 print("list: " + str(vid_arr) + "")
 #base = ffmpeg.input(vid_arr[0])
-base = process_in_groups(vid_arr[0])
+base = process_in_groups(vid_arr[0], 1.45, 800, 7)
 base_v = base['v']
 base_a = base['a']
 for w in range(1, len(vid_arr) - 1):
     # concat = trim_silent(ffmpeg.input(vid_arr[w+1]), w)
-    to_concat = process_in_groups(vid_arr[w])
+    to_concat = process_in_groups(vid_arr[w], 1.45, 800, 7) #1.4, 15000, 5 is pretty good with about 50% retention and near full comprehensibility
     to_concat_v = to_concat['v']
     to_concat_a = to_concat['a']
     print("n = " + str(w))
@@ -173,8 +218,6 @@ for w in range(1, len(vid_arr) - 1):
     base = base.node
     base_v = base['v']
     base_a = base['a']
-base_a = base_a.filter("loudnorm").filter("acompressor") # .filter("dynaudnorm")
-base_v = base_v.filter("atadenoise").filter("saturation").filter("brightness")
 # https://ffmpeg.org/ffmpeg-filters.html#toc-acompressor
 #
 # https://ffmpeg.org/ffmpeg-filters.html#silencedetect
